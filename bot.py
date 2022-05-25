eed41a354173df64ecbda2341a8e5937abeb1aa4
Vasu-xD <81254651+Vasu-xD@users.noreply.github.com>
2022-05-25 08:00:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,7 +40,7 @@ Bot = Client(name="auto-delete",
 #async def start(bot, message):
 #    await message.reply(START_MSG.format(message.from_user.mention))
 
-@User.on_message(filters.chat(GROUP))
+@User.on_message(filters.chat(GROUPS))
 async def delete(user, message):
     try:
        if message.from_user.id in ADMINS:
