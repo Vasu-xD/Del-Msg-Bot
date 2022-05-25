756174e02c599ef22c36eb4968dc8880c8b73dae
Vasu-xD <81254651+Vasu-xD@users.noreply.github.com>
2022-05-25 07:26:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,24 +5,25 @@ from pyrogram import Client, filters, idle
 API_ID = int(environ.get("API_ID"))
 API_HASH = environ.get("API_HASH")
 BOT_TOKEN = environ.get("BOT_TOKEN")
-SESSION = environ.get("SESSION")
+#SESSION = environ.get("SESSION")
 TIME = int(environ.get("TIME"))
-GROUPS = []
-for grp in environ.get("GROUPS").split():
-    GROUPS.append(int(grp))
+BOT_ID = int(environ.get("BOT_ID"))
+#GROUPS = []
+#for grp in environ.get("GROUPS").split():
+#    GROUPS.append(int(grp))
 ADMINS = []
 for usr in environ.get("ADMINS").split():
     ADMINS.append(int(usr))
 
-START_MSG = "<b>Hai {},\nI'm a simple bot to delete group messages after a specific time</b>"
+#START_MSG = "<b>Hai {},\nI'm a simple bot to delete group messages after a specific time</b>"
 
 
-User = Client(name="user-account",
-              session_string=SESSION,
-              api_id=API_ID,
-              api_hash=API_HASH,
-              workers=300
-              )
+#User = Client(name="user-account",
+#              session_string=SESSION,
+#              api_id=API_ID,
+#              api_hash=API_HASH,
+#              workers=300
+#              )
 
 
 Bot = Client(name="auto-delete",
@@ -33,11 +34,11 @@ Bot = Client(name="auto-delete",
              )
 
 
-@Bot.on_message(filters.command('start') & filters.private)
-async def start(bot, message):
-    await message.reply(START_MSG.format(message.from_user.mention))
+#@Bot.on_message(filters.command('start') & filters.private)
+#async def start(bot, message):
+#    await message.reply(START_MSG.format(message.from_user.mention))
 
-@User.on_message(filters.chat(GROUPS))
+@Bot.on_message(filters.chat(BOT_ID))
 async def delete(user, message):
     try:
        if message.from_user.id in ADMINS:
@@ -48,14 +49,14 @@ async def delete(user, message):
     except Exception as e:
        print(e)
        
-User.start()
-print("User Started!")
+#User.start()
+#print("User Started!")
 Bot.start()
 print("Bot Started!")
 
 idle()
 
-User.stop()
-print("User Stopped!")
+#User.stop()
+#print("User Stopped!")
 Bot.stop()
 print("Bot Stopped!")
